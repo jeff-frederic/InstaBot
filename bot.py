c62a6a5749d8149ddcea004001d89ecbf3d0c91f
jeff-PC <jefferyfrederic@gmail.com>
2023-03-02 21:48:27 -0700

    bot

---
+++ bot.py
@@ -0,0 +1,35 @@
+'''
+    Instagram bot that unfollows people that are not following you back.
+
+    Created by @jeff-frederic
+'''
+
+from instabot import Bot
+import os, glob, time, csv
+import numpy as np
+
+USERNAME = ""
+PASSWORD = ""
+
+def load_credentials():
+    credentials = []
+    with open("credentials.csv", "r") as file:
+        credentials = csv.reader(file, delimiter=",", skipinitialspace=True).__next__()
+    USERNAME = credentials[0]
+    PASSWORD = credentials[1]
+    print(USERNAME, PASSWORD)
+
+if __name__ == "__main__":
+    # Remove bot cookies
+    try: os.remove(glob.glob("config/*cookie.json")[0]) 
+    except: pass
+    
+    # Load USERNAME & PASSWORD
+    load_credentials()
+
+    # Initialize instagram bot
+    bot = Bot().console_print
+    bot.login(username=USERNAME, password=PASSWORD, ask_for_code=True)
+    followers = bot.get_user_followers(USERNAME)
+    following = bot.get_user_following(USERNAME)
+    bot.logout()
